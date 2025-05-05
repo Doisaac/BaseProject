b35e9d24e65a43c37e9a6b1ec32e20b8b436238a
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-05-04 19:12:05 -0600

    feat: agregar propiedades fillable al modelo Vehiculo

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Vehiculo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'placa',
+        'marca',
+        'modelo',
+        'año',
+        'estado',
+    ];
 }
