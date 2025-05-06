5ae4640cefdf01aa907d920b0ce800dfc070d5c1
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-05-06 11:55:29 -0600

    feat: agregar script js cdn para el uso correcto de bootstrap en el layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,5 +9,8 @@
     <div class="container mt-4">
         @yield('content')
     </div>
+
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
