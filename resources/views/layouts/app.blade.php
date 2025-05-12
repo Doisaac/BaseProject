4a4de2779931867bf5f87e3e1381b503f9462f4c
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-05-12 16:11:32 -0600

    feat: agregar funcionalidad de búsqueda de imágenes y paginación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'Mi Aplicación')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    @stack('styles')
 </head>
 <body>
     <div class="container mt-4">
@@ -12,5 +13,6 @@
 
     <!-- BOOTSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    @stack('scripts')
 </body>
 </html>
